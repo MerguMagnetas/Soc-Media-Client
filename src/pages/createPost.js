b48f7ac0f1b922bb5312734aaae93b1645aabd77
MerguMagnetas <mykolas.mickus@gmail.com>
2023-06-08 14:39:52 +0300

    Initial commit

--- src/pages/createPost.js
+++ src/pages/createPost.js
@@ -1,38 +1,38 @@
-import React, { useContext, useEffect } from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../helpers/AuthContext";
+import React, { useContext, useEffect } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../helpers/AuthContext';
 
 function CreatePost() {
   const { authState } = useContext(AuthContext);
 
-  let navigate = useNavigate();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (!localStorage.getItem("accessToken")) {
-      navigate("/login");
+    if (!localStorage.getItem('accessToken')) {
+      navigate('/login');
     }
   }, [navigate]);
 
   const initialValues = {
-    title: "",
-    postText: "",
+    title: '',
+    postText: '',
   };
 
   const validationSchema = Yup.object().shape({
-    title: Yup.string().required("You must input a Title!"),
-    postText: Yup.string().required("You must input a Post!"),
+    title: Yup.string().required('You must input a Title!'),
+    postText: Yup.string().required('You must input a Post!'),
   });
 
   const onSubmit = (data) => {
     axios
-      .post("http://localhost:3001/posts", data, {
-        headers: { accessToken: localStorage.getItem("accessToken") },
+      .post('http://localhost:3001/posts', data, {
+        headers: { accessToken: localStorage.getItem('accessToken') },
       })
       .then((response) => {
-        navigate("/");
+        navigate('/');
       });
   };
 
@@ -60,7 +60,7 @@ function CreatePost() {
             name="postText"
             placeholder="(Ex. Post...)"
           />
-          <button type="submit"> Create Post</button>
+          <button type="submit">Create Post</button>
         </Form>
       </Formik>
     </div>
